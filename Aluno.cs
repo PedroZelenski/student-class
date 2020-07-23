de92144b6f41c2ae3fbcfb4e0a1d0e1392704e15
Zelenski <zelenski.contato@gmail.com>
2020-07-23 15:03:28 -0300

    first commit

---
+++ Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace student_class
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Sala { get; set; }
+       
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+        public double Nota4 { get; set; }
+
+        public void Notas()
+        {
+            Console.WriteLine("Digite a primeira nota: ");
+            Nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota: ");
+            Nota2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a terceira nota: ");
+            Nota3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a quarta nota: ");
+            Nota4 = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void MediaFinal()
+        {
+            double media = (Nota1 + Nota2 + Nota3 + Nota4) / 4;
+            Console.WriteLine($"A MEDIA E: {media}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace student_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            aluno.Notas();
+            aluno.MediaFinal();
+        }
+    }
+}
